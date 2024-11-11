e3b90e28272e599c988de1817a5b3b49f3f8480b
Gustavo Garcia <gustavogarcia@sejaimpulse.com>
2024-11-11 08:40:04 -0300

    feat: dashboard style

--- src/app/dashboard/_components/dashboard-sidebar.tsx
+++ src/app/dashboard/_components/dashboard-sidebar.tsx
@@ -1,4 +1,21 @@
+import {
+  BoxIcon,
+  ChartColumn,
+  Contact,
+  CreditCard,
+  Home,
+  Package2,
+  PieChartIcon,
+  Receipt,
+  ShieldQuestion,
+  TrendingUpIcon,
+  UserRound,
+  Users2,
+  UserSearch,
+} from 'lucide-react'
+
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Badge } from '@/components/ui/badge'
 import {
   Sidebar,
   SidebarContent,
@@ -11,7 +28,6 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from '@/components/ui/sidebar'
-import { BoxIcon, CreditCard, Home, Package2, Receipt, Settings2, UserRound, Users2 } from 'lucide-react'
 
 const applicationMenu = [
   {
@@ -51,18 +67,55 @@ const settingsMenu = [
     label: 'Subscription',
     icon: CreditCard,
     link: '/dashboard/settings/subscription',
-  }
+  },
 ]
- 
+
+const analyticsMenu = [
+  {
+    label: 'Overview',
+    icon: PieChartIcon,
+    link: '/dashboard/analytics/overview',
+  },
+  {
+    label: 'Your reports',
+    icon: ChartColumn,
+    link: '/dashboard/analytics/your',
+  },
+  {
+    label: 'Career',
+    icon: TrendingUpIcon,
+    link: '/dashboard/analytics/career',
+  },
+]
+
+const helpMenu = [
+  {
+    label: 'User Guide',
+    icon: UserSearch,
+    link: '/dashboard/help/user-guide',
+  },
+  {
+    label: 'FAQ',
+    icon: ShieldQuestion,
+    link: '/dashboard/help/faq',
+  },
+  {
+    label: 'Contact Support',
+    icon: Contact,
+    link: '/dashboard/help/contact',
+  },
+]
+
 export function DashboardSidebar() {
   return (
     <Sidebar>
       <SidebarHeader>
-         <div className="flex min-h-10 items-center justify-center border-b bg-theme-dark">
-           <h2 className="text-center text-2xl font-bold uppercase text-theme-light">
-             Delivero
-           </h2>
-         </div>
+        <div className="flex min-h-10 flex-row items-center justify-center gap-2 border-b bg-theme-dark">
+          <h2 className="text-center text-2xl font-bold uppercase text-theme-light">
+            Delivero
+          </h2>
+          <Badge>Deliverer</Badge>
+        </div>
       </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
@@ -83,9 +136,24 @@ export function DashboardSidebar() {
           </SidebarGroupContent>
         </SidebarGroup>
         <SidebarGroup>
-          <SidebarGroupLabel>
-            Settings
-          </SidebarGroupLabel>
+          <SidebarGroupLabel>Analytics</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {analyticsMenu.map((item) => (
+                <SidebarMenuItem key={item.label}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.link}>
+                      <item.icon />
+                      <span>{item.label}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel>Settings</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
               {settingsMenu.map((item) => (
@@ -101,20 +169,40 @@ export function DashboardSidebar() {
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
+
+        <SidebarGroup className="mt-auto">
+          <SidebarGroupLabel>Help & Support</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {helpMenu.map((item) => (
+                <SidebarMenuItem key={item.label}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.link}>
+                      <item.icon />
+                      <span>{item.label}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
       </SidebarContent>
 
       <SidebarFooter>
-        <div className='flex flex-row gap-2 items-center text-theme-light justify-between border-t p-4'>
-          <div className='flex flex-col gap-px'>
-            <p className='font-semibold text-lg'>Hello, user!</p>
-            <small className='text-xs text-theme-light/60'>Switch account</small>
+        <div className="flex flex-row items-center justify-between gap-2 border-t p-4 text-theme-light">
+          <div className="flex flex-col gap-px">
+            <p className="text-lg font-semibold">Hello, user!</p>
+            <small className="text-xs text-theme-light/60">
+              Switch account
+            </small>
           </div>
 
           <Avatar>
             <AvatarFallback>
               <UserRound />
             </AvatarFallback>
-            <AvatarImage src='https://github.com/gustavogarciac.png' />
+            <AvatarImage src="https://github.com/gustavogarciac.png" />
           </Avatar>
         </div>
       </SidebarFooter>
